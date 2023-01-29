69698c8c3dce961c1000208c406b84ce2f97cdc8
FGoosev <fgoosev@gmail.com>
2023-01-29 12:37:31 +0400

    Initial Commit

---
+++ ApiPlayer/ApiPlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ApiPlayerApp.swift
+//  ApiPlayer
+//
+//  Created by Александр Гусев on 29.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct ApiPlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
